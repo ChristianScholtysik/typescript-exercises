0536de3c3c384b948f3d757800e6884892a3b39e
Christian Scholtysik <christianscholtysik@gmail.com>
2024-05-02 17:20:55 +0200

    done

--- src/03_exercises/math.ts
+++ src/03_exercises/math.ts
@@ -21,13 +21,24 @@ console.log(Math.floor(randomNum1_100));
 //! Math-TS-Level-2_2
 
 const randomNumGame = Math.floor(Math.random() * 10);
-// console.log(randomNumGame);
+console.log(randomNumGame);
 
 const userInputGame = window.prompt(" Insert a  number from 1-10");
 const userInputGameNumber: number = Number(userInputGame);
 
-if (userInputGameNumber === randomNumGame) {
-  console.log("Du bist der Beste");
-} else {
-  console.log("Versuche es noch einmal. Die Zahl lautet: ", randomNumGame);
-}
+// if (userInputGameNumber === randomNumGame) {
+//   console.log("Du bist der Beste");
+// } else {
+//   console.log("Versuche es noch einmal. Die Zahl lautet: ", randomNumGame);
+// }
+
+const userGuess = document.getElementById("ergebnis");
+if (userGuess)
+  if (userInputGameNumber === randomNumGame) {
+    userGuess.innerHTML = "Du bist der Beste!!!";
+    userGuess.style.color = "red";
+    userGuess.style.fontSize = "7rem";
+  } else {
+    userGuess.innerHTML =
+      "Versuche es noch einmal. Die Zahl lautet:" + randomNumGame;
+  }
